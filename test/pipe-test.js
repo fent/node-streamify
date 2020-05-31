03d4b90a0c8e0885910cef0934d02147ba62823a
fent <933490+fent@users.noreply.github.com>
2020-05-30 21:58:34 -0400

    test: use await for `stream-equal`

--- test/pipe-test.js
+++ test/pipe-test.js
@@ -10,60 +10,51 @@ const input = path.join(__dirname, 'files', 'input1.txt');
 
 
 describe('Pipe from a readable stream', () => {
-  it('Stream `data` events equal to original stream', (done) => {
+  it('Stream `data` events equal to original stream', async () => {
     const writeStream = new PassThrough();
     const readStream = fs.createReadStream(input, { bufferSize: 1024 });
     const stream = streamify();
     stream.pipe(writeStream);
 
-    streamEqual(readStream, writeStream, (err, equal) => {
-      assert.ifError(err);
-      assert.ok(equal);
-      done();
-    });
-
     setTimeout(() => {
       let fileReadStream = fs.createReadStream(input, { bufferSize: 1024 });
       stream.resolve(fileReadStream);
     }, 10);
+
+    let equal = await streamEqual(readStream, writeStream);
+    assert.ok(equal);
   });
 });
 
 describe('Pipe to a writable stream', () => {
-  it('Everything written to final stream', (done) => {
+  it('Everything written to final stream', async () => {
     const readStream = fs.createReadStream(input, { bufferSize: 1024 });
     const writeStream = new PassThrough();
     const stream = streamify();
     fs.createReadStream(input, { bufferSize: 1024 }).pipe(stream);
 
-    streamEqual(readStream, writeStream, (err, equal) => {
-      assert.ifError(err);
-      assert.ok(equal);
-      done();
-    });
-
     setTimeout(() => {
       stream.resolve(writeStream);
     }, 10);
+
+    let equal = await streamEqual(readStream, writeStream);
+    assert.ok(equal);
   });
 });
 
 describe('Pipe to itself', () => {
-  it('Everything from source stream written to dest stream', (done) => {
+  it('Everything from source stream written to dest stream', async () => {
     const readStream = fs.createReadStream(input);
     const writeStream = new PassThrough();
     const stream = streamify();
     stream.pipe(stream);
 
-    streamEqual(readStream, writeStream, (err, equal) => {
-      assert.ifError(err);
-      assert.ok(equal);
-      done();
-    });
-
     setTimeout(() => {
       stream.addSource(fs.createReadStream(input));
       stream.addDest(writeStream);
     }, 10);
+
+    let equal = await streamEqual(readStream, writeStream);
+    assert.ok(equal);
   });
 });
